f901e3199907b905ea9460bd0937d83c70a9feda
Pavan Shashidharan <pavanshashidharan@gmail.com>
2016-09-07 16:51:09 +1200

    Table filters added

--- app/models/article.rb
+++ app/models/article.rb
@@ -40,7 +40,7 @@ class Article < ActiveRecord::Base
     end
   }
 
-  # pavan simple search
+  # pavan primary search in the search page
   def self.search(search)
     if search
       where(["title LIKE ?","%#{search}%"])
