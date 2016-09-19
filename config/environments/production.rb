780ee7d8670b0e7fb5b1c04d478749ce61028ccd
Trung Tang <tdbt1985@gmail.com>
2016-09-19 22:21:45 +1200

    Update email config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-   config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
- config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    address:              'smtp.gmail.com',
@@ -91,5 +91,5 @@ config.action_mailer.raise_delivery_errors = true
    password:             'sdmstream2',
    authentication:       'plain',
    enable_starttls_auto: true  
-}a
+}
 end
