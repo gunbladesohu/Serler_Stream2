c28eced642f1620163fd2324aef3fa2a3c9d0787
Trung Tang <tdbt1985@gmail.com>
2016-09-19 22:18:46 +1200

    config email for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+   config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+ config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                 587,
+   domain:               'gmail.com',
+   user_name:            'serlerstream2@gmail.com',
+   password:             'sdmstream2',
+   authentication:       'plain',
+   enable_starttls_auto: true  
+}a
 end
