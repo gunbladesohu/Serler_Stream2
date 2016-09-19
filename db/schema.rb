8c7ac930382c043cacfd50c91a4d768b491d1882
Trung Tang <tdbt1985@gmail.com>
2016-09-19 21:55:21 +1200

    finish merge with CoDoctors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906020304) do
+ActiveRecord::Schema.define(version: 20160917072029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(version: 20160906020304) do
     t.boolean  "is_active"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.boolean   "peer_reviewed"
-    t.boolean   "relevance"
-    t.boolean   "not_duplicate"
-    t.integer   "user_id"
+    t.boolean  "peer_reviewed"
+    t.boolean  "relevance"
+    t.boolean  "not_duplicate"
+    t.integer  "user_id"
   end
 
   create_table "articles_authors", force: :cascade do |t|
