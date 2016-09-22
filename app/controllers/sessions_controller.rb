d4e0c0444de095210ba0f147f99b5ee34bfdf5e5
Athena <hit815@163.com>
2016-09-22 15:45:28 +1200

    google account login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,10 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
+
       redirect_to user
+
+
     else
       # Create an error message.
       flash.now[:notice] = 'Invalid email/password combination'
